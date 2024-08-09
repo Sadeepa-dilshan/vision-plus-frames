8b24a985e2ab139017e46ca666049bdc74380de3
lahirusb97 <lahirushirant@gmail.com>
2024-08-09 23:08:47 +0530

    nav bar updaye

--- laravel_react_project/src/Components/DefaultLayout.jsx
+++ laravel_react_project/src/Components/DefaultLayout.jsx
@@ -5,49 +5,30 @@ import { Navigate, Outlet } from "react-router-dom";
 import axiosClient from "../axiosClient";
 import { useStateContext } from "../contexts/contextprovider";
 import { Link } from "react-router-dom";
+import NavBar from "./NavBar";
+export default function DefaultLayout() {
+    const { user, token, setUser, setToken } = useStateContext();
+    if (!token) {
+        return <Navigate to="/login" />;
+    }
 
-export default function DefaultLayout(){
-    const {user, token, setUser, setToken} = useStateContext();
-    if(!token){
-       return <Navigate to='/login'/>
-    }
-    
-    const onLogout =  (ev) =>{
+    const onLogout = (ev) => {
         ev.preventDefault();
-        axiosClient.get('/logout')
-        .then(({}) => {
-           setUser(null)
-           setToken(null)
-        })
-    }
+        axiosClient.get("/logout").then(({}) => {
+            setUser(null);
+            setToken(null);
+        });
+    };
 
     useEffect(() => {
-        axiosClient.get('/user')
-          .then(({data}) => {
-             setUser(data)
-          })
-      }, [])
+        axiosClient.get("/user").then(({ data }) => {
+            setUser(data);
+        });
+    }, []);
 
-    return(
+    return (
         <div id="defaultLayout">
-         <div className="content">
-            <header>
-                <div>
-                    Header
-                </div>
-                <div>
-                        <Link to="/brands">View All Brands</Link> {/* Link to the brand index page */}
-                        <Link to="/brands/new">Create New Brand</Link> {/* Link to create a new brand */}
-                    </div>
-                <div>
-                    {user.name}
-                    <a href="#" onClick={onLogout} className="btn-logout"> Logout</a>
-                </div>
-            </header>
-            <main>
-            <Outlet />
-            </main>
-            </div>
+            <NavBar />
         </div>
-    )
+    );
 }
