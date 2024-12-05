07058485cb62c73ae9866a1c98e89686cfe3c6bb
sadeepa-dilshan <s19002535@ousl.lk>
2024-12-05 19:10:10 +0530

    lenses low

--- routes/api.php
+++ routes/api.php
@@ -98,6 +98,7 @@ Route::apiResource('lens-coatings', CoatingController::class);
 Route::apiResource('lens-powers', LensPowersController::class)->only('index','store','delete','show');
 Route::put('lens-powers/update-multiple', [LensPowersController::class, 'updateMultiple']);
 Route::get('top-lenses-by-stock-reduction', [LensController::class, 'toplensesByStockReduction']);
+Route::get('low-lenses-by-stock-reduction', [LensController::class, 'lowPerformingLensesByStockReduction']);
 
 Route::post('login',[AuthController::class,'login']);
 Route::post('register',[AuthController::class,'register']);
