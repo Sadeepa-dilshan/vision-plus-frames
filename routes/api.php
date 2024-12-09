e91ffbb673e0aeb9fc66ef4d13f8d96d505260b1
sadeepa-dilshan <s19002535@ousl.lk>
2024-12-09 21:52:08 +0530

    stock qties

--- routes/api.php
+++ routes/api.php
@@ -27,7 +27,7 @@ use App\Http\Controllers\LensPowersController;
 |
 */
 
-Route::middleware('auth:sanctum')->group(function() {
+// Route::middleware('auth:sanctum')->group(function() {
     Route::get('logout',[AuthController::class,'logout']);
     Route::get('/user', function (Request $request) {
         return $request->user();
@@ -35,46 +35,39 @@ Route::middleware('auth:sanctum')->group(function() {
     Route::middleware('admin')->group(function () {
         Route::apiResource('/users', UserController::class)->except(['show', 'update']);
     });
-});
-// Brands Routes
-Route::middleware(['auth:sanctum'])->group(function() {
+
+    // Brands Routes
     Route::get('brands', [BrandController::class, 'index']);
     Route::post('brands', [BrandController::class, 'store'])->middleware('admin');
     Route::get('brands/{brand}', [BrandController::class, 'show']);
     Route::put('brands/{brand}', [BrandController::class, 'update'])->middleware('admin');
     Route::delete('brands/{brand}', [BrandController::class, 'destroy'])->middleware('admin');
-});
 
-// Codes Routes
-Route::middleware(['auth:sanctum'])->group(function() {
+    // Codes Routes
     Route::get('codes', [CodeController::class, 'index']);
     Route::post('codes', [CodeController::class, 'store'])->middleware('admin');
     Route::get('codes/{code}', [CodeController::class, 'show']);
     Route::put('codes/{code}', [CodeController::class, 'update'])->middleware('admin');
     Route::delete('codes/{code}', [CodeController::class, 'destroy'])->middleware('admin');
-});
 
-// Colors Routes
-Route::middleware(['auth:sanctum'])->group(function() {
+    // Colors Routes
     Route::get('colors', [ColorController::class, 'index']);
     Route::post('colors', [ColorController::class, 'store'])->middleware('admin');
     Route::get('colors/{color}', [ColorController::class, 'show']);
     Route::put('colors/{color}', [ColorController::class, 'update'])->middleware('admin');
     Route::delete('colors/{color}', [ColorController::class, 'destroy'])->middleware('admin');
-});
 
-// Frames Routes
-Route::middleware(['auth:sanctum'])->group(function() {
+    // Frames Routes
     Route::get('frames', [FrameController::class, 'index']);
     Route::post('frames', [FrameController::class, 'store'])->middleware('admin');
     Route::get('frames/{frame}', [FrameController::class, 'show']);
     Route::post('frames/{frame}', [FrameController::class, 'update'])->middleware('admin');
     Route::delete('frames/{frame}', [FrameController::class, 'destroy'])->middleware('admin');
     Route::get('top-frames-by-stock-reduction', [FrameController::class, 'topFramesByStockReduction']);
-});
 
-// Stocks Routes
-// Route::middleware(['auth:sanctum'])->group(function() {
+    // Stocks Routes
+    Route::get('stocks/all-stock', [StockController::class, 'allStock']);
+    Route::get('stocks/total-sold', [StockController::class, 'totalSoldQty']);
     Route::get('stocks', [StockController::class, 'index']);
     Route::post('stocks', [StockController::class, 'store'])->middleware('admin');
     Route::get('stocks/{stock}', [StockController::class, 'show']);
@@ -90,23 +83,24 @@ Route::middleware(['auth:sanctum'])->group(function() {
     Route::put('lens-stocks/{stock}', [LensStockController::class, 'update']);
     Route::delete('lens-stocks/{stock}', [LensStockController::class, 'destroy'])->middleware('admin');
     Route::get('/lenses/{lensId}/stock-history', [LensStockController::class, 'getStockHistory']);
+
+    Route::apiResource('lenses', LensController::class);
+    Route::apiResource('lens-types', LensTypeController::class);
+    Route::apiResource('lens-coatings', CoatingController::class);
+    Route::apiResource('lens-powers', LensPowersController::class)->only('index','store','delete','show');
+    Route::put('lens-powers/update-multiple', [LensPowersController::class, 'updateMultiple']);
+    Route::get('top-lenses-by-stock-reduction', [LensController::class, 'toplensesByStockReduction']);
+    Route::get('low-lenses-by-stock-reduction', [LensController::class, 'lowPerformingLensesByStockReduction']);
+
+    Route::post('login',[AuthController::class,'login']);
+    Route::post('register',[AuthController::class,'register']);
+    Route::get('hello', function () {
+        return response()->json(['message' => 'Hello World ,App Works!...']);
+    });
+    Route::get('top-frames-by-stock-reduction', [FrameController::class, 'topFramesByStockReduction']);
+    Route::get('/frames/{frameId}/stock-history', [StockController::class, 'getStockHistory']);
+
+    //branches
+    Route::apiResource('branches', BranchController::class);
+
 // });
-
-Route::apiResource('lenses', LensController::class);
-Route::apiResource('lens-types', LensTypeController::class);
-Route::apiResource('lens-coatings', CoatingController::class);
-Route::apiResource('lens-powers', LensPowersController::class)->only('index','store','delete','show');
-Route::put('lens-powers/update-multiple', [LensPowersController::class, 'updateMultiple']);
-Route::get('top-lenses-by-stock-reduction', [LensController::class, 'toplensesByStockReduction']);
-Route::get('low-lenses-by-stock-reduction', [LensController::class, 'lowPerformingLensesByStockReduction']);
-
-Route::post('login',[AuthController::class,'login']);
-Route::post('register',[AuthController::class,'register']);
-Route::get('hello', function () {
-    return response()->json(['message' => 'Hello World ,App Works!...']);
-});
-Route::get('top-frames-by-stock-reduction', [FrameController::class, 'topFramesByStockReduction']);
-Route::get('/frames/{frameId}/stock-history', [StockController::class, 'getStockHistory']);
-
-//branches
-Route::apiResource('branches', BranchController::class);
